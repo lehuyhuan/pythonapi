fc01892954dbb3997dc17a0d20c13051696e17ff
Huy Huan <huy.huan@ericsson.com>
2022-12-08 23:27:59 +0700

    add project

---
+++ api.py
@@ -0,0 +1,15 @@
+from flask import Flask, json, request
+
+api = Flask(__name__)
+
+@api.route('/sum', methods=['GET'])
+def birthday():
+    vms=request.args.get('birthday')
+    return json.dumps(list(vms))
+    
+@api.route('/about', methods=['GET'])
+def hi():
+    return "This is simple api, copyright @lehuyhuan @V_1.0.0"
+
+if __name__ == '__main__':
+    api.run(host='0.0.0.0',port=5000)
